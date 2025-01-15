04c50aab7fa5331af5757005dc8e95cf3bbd3225
Jahnear Isaiah Faiquare Vizcaíno <jfaiquare@unal.edu.co>
2025-01-15 00:59:18 -0500

    Update Alfabeto.java

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -1,40 +1,32 @@
 package comunicacion;
 
-public class Alfabeto extends Escrito {
+public class Alfabeto extends Pictograma {
     private String[] letras;
     private String interpretacion;
-
+    
     public Alfabeto(String origen, String[] letras, String interpretacion) {
-        super(origen, "", "", 0);
+        super(origen);
         this.letras = letras;
         this.interpretacion = interpretacion;
     }
-
+    
     public int cantidadLetras() {
         return letras.length;
     }
-
+    
+    public String[] getLetras() {
+        return letras;
+    }
+    
+    public void setLetras(String[] letras) {
+        this.letras = letras;
+    }
+    
     @Override
     public String interpretacion() {
         return interpretacion;
     }
-
-    public String[] getLetras() {
-        return letras;
-    }
-
-    public void setLetras(String[] letras) {
-        this.letras = letras;
-    }
-
-    public String getInterpretacion() {
-        return interpretacion;
-    }
-
-    public void setInterpretacion(String interpretacion) {
-        this.interpretacion = interpretacion;
-    }
-
+    
     @Override
     public String toString() {
         return String.join(", ", letras);
